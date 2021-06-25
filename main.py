4cb8fcc5946328347afa6cff72146d99e302115a
SolutionsAI <86385390+SolutionsAI@users.noreply.github.com>
2021-06-25 19:34:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ def detect():
     st.image('foto.jpeg', caption='ejemplo de esperma por funcion')
     
 
+b = False
 
 x = st.slider('x')  # 👈 this is a widget
 st.write(x, 'squared is', x * x)
 
-b = False
 B1 = st.button('Detectar')
 B2 = st.button('No Detectar')
 
