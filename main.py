07ddf5f120b53726401819ad6350e1fb603ad733
SolutionsAI <86385390+SolutionsAI@users.noreply.github.com>
2021-06-25 20:26:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ uploaded_files = st.file_uploader("Cargar Imágenes", accept_multiple_files=True
 for uploaded_file in uploaded_files:
      bytes_data = uploaded_file.read()
      st.write("filename:", uploaded_file.name)
-     st.write(bytes_data)
+     st.image(uploaded_file)
     
     
  
