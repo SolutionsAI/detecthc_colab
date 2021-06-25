7932de4eae591c62f6749e042a9c786d0288f1a4
SolutionsAI <86385390+SolutionsAI@users.noreply.github.com>
2021-06-25 19:13:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ st.write(pd.DataFrame({
 
 def detect():
     print("Hello from a function")
-    st.image('foto.jpeg', caption='ejemplo de esperma')
+    st.image('foto.jpeg', caption='ejemplo de esperma por funcion')
     
 detect()
 
