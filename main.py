517f255d87199168aec1b9077d916ee09ee7b34e
SolutionsAI <86385390+SolutionsAI@users.noreply.github.com>
2021-06-25 19:25:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,6 @@ def detect():
 x = st.slider('x')  # 👈 this is a widget
 st.write(x, 'squared is', x * x)
 
-if st.button('Say hello'):
-    st.write('Why hello there')
-else:
-    st.write('Goodbye')
+if st.button('Detectar'):
     detect()
+    
