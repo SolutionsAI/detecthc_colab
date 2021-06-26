c3b24a0127910c87076da91f767d82b0285be34b
SolutionsAI <86385390+SolutionsAI@users.noreply.github.com>
2021-06-26 20:18:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,8 @@ y = [2,6,2,9,7,1,5,7,5,2,8]
 x = [1,2,3,4,5,6,7,8,9,10,11]
 
 chart_data = pd.DataFrame(
-     np.random.randn(5, 5),
+     #np.random.randn(5, 5),
+     [70,50,100,78,64],
      columns=['H+', 'H-', 'C+', 'C-', 'NC'])
 st.bar_chart(chart_data)   
 
