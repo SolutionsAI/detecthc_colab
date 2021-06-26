6d67cbec9ba371fcf5f4b4ed21d32d37244ffa9f
SolutionsAI <86385390+SolutionsAI@users.noreply.github.com>
2021-06-26 16:49:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,72 +7,42 @@ import os
 from matplotlib import pyplot as plt
 import time
 
+
+#SIDEBAR stuff---------------------------------
+#imagen
+st.sidebar.image('foto.jpeg', caption='Detector y clasificador de espermatozoides')
+#umbral
+st.sidebar.write("umbral de confianza")
+e = st.sidebar.sidebar.slider('x')  
+st.write(e)
+#----------------------------------------------
+
+#titulo
 st.title('Detect HOS Coomassie Sperm Solutions')
 
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+#descripcion
+st.write("Seleccione el conjunto de imagenes a segmentar y clasificar")
 
-
-
-def detect():
-    print("Hello from a function")
-    st.sidebar.image('foto.jpeg', caption='ejemplo de esperma por funcion')
-    
-
-b = False
-
-e = st.sidebar.slider('x')  # 👈 this is a widget
-st.write(e, 'squared is', e * e)
-
-
-
-
-if st.sidebar.button('Detectar'):
-    b = not b
-    
-    
-st.write(b)
-
-if b:
-    detect()
-    
-option = st.selectbox(
-     'How would you like to be contacted?',
-     ('Email', 'Home phone', 'Mobile phone'))
-
-st.write('You selected:', option)
-
-y = [2,6,2,9,7,1,5,7,5,2,8] 
-x = [1,2,3,4,5,6,7,8,9,10,11]
-
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
-
-st.line_chart(chart_data)
-
+#browse files
 uploaded_files = st.file_uploader("Cargar Imágenes", accept_multiple_files=True)
 cont = 0
-my_bar = st.progress(0)
-t = len(uploaded_files)
-if t != 0:
-    inc = 100/t
-else:
-    inc = 0
 for uploaded_file in uploaded_files:
      
      cont = cont + inc
      #bytes_data = uploaded_file.read()
      #st.write("filename:", uploaded_file.name)
      #st.image(detect(uploaded_file))
-     my_bar.progress(cont)
-        
 st.write('Imagenes Cargadas: ',cont)
-if b:
-    detect()
+
+
+#grafico de resultdos
+y = [2,6,2,9,7,1,5,7,5,2,8] 
+x = [1,2,3,4,5,6,7,8,9,10,11]
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 5),
+     columns=['H+', 'H-', 'C+', 'C-', 'NC'])
+st.bar_chart(chart_data)    
     
     
  
