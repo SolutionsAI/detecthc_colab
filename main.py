c93edaaba82fe616f5e1d713f282d6abc2491edb
SolutionsAI <86385390+SolutionsAI@users.noreply.github.com>
2021-06-30 20:12:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from PIL import Image
+import cv2 
 # To make things easier later, we're also importing numpy and pandas for
 # working with sample data.
 import numpy as np
@@ -11,12 +13,12 @@ import time
 import torch
 import torch.backends.cudnn as cudnn
 
-from models.experimental import attempt_load
-from utils.datasets import LoadStreams, LoadImages
-from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
-    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path, save_one_box
-from utils.plots import colors, plot_one_box
-from utils.torch_utils import select_device, load_classifier, time_synchronized
+#from models.experimental import attempt_load
+#from utils.datasets import LoadStreams, LoadImages
+#from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+#    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path, save_one_box
+#from utils.plots import colors, plot_one_box
+#from utils.torch_utils import select_device, load_classifier, time_synchronized
 
 
 
