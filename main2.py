32fed3fd1ad1769f192bdcb0e2d0a148d334eff1
SolutionsAI <86385390+SolutionsAI@users.noreply.github.com>
2021-06-30 22:31:07 -0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -33,12 +33,13 @@ st.write("Seleccione el conjunto de imágenes a segmentar y clasificar")
 
 #browse files
 uploaded_files = st.file_uploader("Cargar Imágenes", accept_multiple_files=True)
+st.write(uploaded_files)
 i = 0
 for uploaded_file in uploaded_files:
-     #h = cv2.imread("foto.jpeg")
-     h =uploaded_file.read()
+     h = cv2.imread("foto.jpeg")
+     
      i = i + 1
-     st.write(h)
+     #st.write(h)
      cv2.imwrite("img"+str(i)+".jpeg",h)
      #bytes_data = uploaded_file.read()
      #st.write("filename:", uploaded_file.name)
