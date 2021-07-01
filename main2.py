7fa7321d380c528b7207a5bfd7358479f14c9326
SolutionsAI <86385390+SolutionsAI@users.noreply.github.com>
2021-06-30 23:02:57 -0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -37,8 +37,9 @@ uploaded_files = st.file_uploader("Cargar Imágenes", accept_multiple_files=True
 i = 0
 for uploaded_file in uploaded_files:
      h = cv2.imread("foto.jpeg")
-     bytes_data = uploaded_file.getvalue().decode("utf-8")
-     st.write(bytes_data)
+     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+     st.write(stringio)
+     
      i = i + 1
      #st.write(uploaded_files)
      cv2.imwrite("img"+str(i)+".jpeg",h[:,:,::-1])
