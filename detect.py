2505e64c15c35601e57b10b9376747d715dd9727
SolutionsAI <86385390+SolutionsAI@users.noreply.github.com>
2021-07-01 19:21:45 -0300

    Update detect.py

--- detect.py
+++ detect.py
@@ -28,7 +28,7 @@ def mostrar(img,titulo=""):
 #=================== FUNCION PARA COOMASSIE ========================================================================================================================
 
 @torch.no_grad()
-def detectCOM(weights='yolov5s.pt',  # model.pt path(s)
+def detectCOM(weights='yolov5x.pt',  # model.pt path(s)
            source='data/images',  # file/dir/URL/glob, 0 for webcam
            imgsz=640,  # inference size (pixels)
            conf_thres=0.2,  # confidence threshold
@@ -135,8 +135,8 @@ def detectCOM(weights='yolov5s.pt',  # model.pt path(s)
 #VER LINEA 144
 
 @torch.no_grad()
-def detect(weights='yolov5s.pt',  # model.pt path(s)
-           source='data/images',  # file/dir/URL/glob, 0 for webcam
+def detectHOS(weights='yolov5x.pt',  # model.pt path(s)
+           source='burro.jpg',  # file/dir/URL/glob, 0 for webcam
            imgsz=640,  # inference size (pixels)
            conf_thres=0.4,  # confidence threshold
            iou_thres=0.5,  # NMS IOU threshold
@@ -338,37 +338,37 @@ def detect(weights='yolov5s.pt',  # model.pt path(s)
 
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='data/images', help='file/dir/URL/glob, 0 for webcam')
-    parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.4, help='confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.5, help='NMS IoU threshold')
-    parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true', help='show results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
-    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
-    parser.add_argument('--name', default='exp', help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
-    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
-    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
-    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
-    opt = parser.parse_args()
-    print(opt)
-    check_requirements(exclude=('tensorboard', 'thop'))
-
-    h = detect(**vars(opt))
-
-    print("ESTO DIO AL FINAL:",h)
+#if __name__ == '__main__':
+#    parser = argparse.ArgumentParser()
+#    parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
+#    parser.add_argument('--source', type=str, default='data/images', help='file/dir/URL/glob, 0 for webcam')
+#    parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='inference size (pixels)')
+#    parser.add_argument('--conf-thres', type=float, default=0.4, help='confidence threshold')
+#    parser.add_argument('--iou-thres', type=float, default=0.5, help='NMS IoU threshold')
+#    parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
+#    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+#   parser.add_argument('--view-img', action='store_true', help='show results')
+#   parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+#   parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+#   parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
+#   parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+#   parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+#    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+#    parser.add_argument('--augment', action='store_true', help='augmented inference')
+#    parser.add_argument('--update', action='store_true', help='update all models')
+#    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+#    parser.add_argument('--name', default='exp', help='save results to project/name')
+#    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+#    parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
+#    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
+#    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
+#    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
+#    opt = parser.parse_args()
+#    print(opt)
+#    check_requirements(exclude=('tensorboard', 'thop'))
+#
+#   h = detect(**vars(opt))
+#
+#    print("ESTO DIO AL FINAL:",h)
     
 
