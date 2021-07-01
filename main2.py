969d4ca7c517a3b44aa904c9bd81fcfc0e5c8e47
SolutionsAI <86385390+SolutionsAI@users.noreply.github.com>
2021-07-01 19:52:04 -0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -77,8 +77,13 @@ if i:
      j = detectHOS()
      st.write(j)
      img = cv2.imread("runs/detect/exp/burro.jpg")
+     st.image(img[:,:,::-1])
      
-     st.image(img)
+     img = cv2.imread("runs/detect/exp2/burro.jpg")
+     st.image(img[:,:,::-1])
+     
+     img = cv2.imread("runs/detect/exp3/burro.jpg")
+     st.image(img[:,:,::-1])
      #st.write("IMAGEN ORIGINAL")
      #h = cv2.imread("foto.jpeg")
      #st.image(h)
