ebc386e230fe81741d70548665bd543e12a03119
SolutionsAI <86385390+SolutionsAI@users.noreply.github.com>
2021-07-01 22:19:11 -0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -15,8 +15,10 @@ from detect import *
 
 from streamlit import caching
 caching.clear_cache()
+from IPython import get_ipython
+get_ipython().magic('reset -sf')
 
-rmtree('app/detecthc')
+#rmtree('app/detecthc/')
 
 #SIDEBAR stuff---------------------------------
 #imagen
