7bb8a98b6815713504388ed68d3e6185d3d36b50
SolutionsAI <86385390+SolutionsAI@users.noreply.github.com>
2021-07-20 20:05:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ st.write('Haga click en "Comenzar" para realizar el proceso de detección y clas
 i = st.button("Comenzar")
 if i:
      #st.write(os.getcwd()) #para ver el directorio
-     Repo.clone_from("https://gitlab.com/iasolutions_arg/detecthc_weights.git", "/app/detecthc")
+     Repo.clone_from("https://gitlab.com/iasolutions_arg/detecthc_weights.git", "/app/detecthc/pesos")
      #j = detectHOS(C/100)
      #st.write(j)
      #img = cv2.imread("runs/detect/exp/burro.jpg")
