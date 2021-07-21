ff6d28b60d055490dbf450a6986e729fab4a3667
SolutionsAI <86385390+SolutionsAI@users.noreply.github.com>
2021-07-21 19:41:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ if i:
           Repo.clone_from("https://gitlab.com/iasolutions_arg/detecthc_weights.git", "/app/detecthc/pesos")
           
      #st.write(os.getcwd()) #para ver el directorio
-     j = detectHOS(C/100,uploaded_files)
+     j = detectHOS(C/100)
      st.write(j)
      img = cv2.imread("runs/detect/exp/burro.jpg")
      st.image(img[:,:,::-1])
