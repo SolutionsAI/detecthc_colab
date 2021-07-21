d37806f3af14265e6332fa59a709ef58fbf04333
SolutionsAI <86385390+SolutionsAI@users.noreply.github.com>
2021-07-21 18:36:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ st.title('Detector y clasificador HOS-Coomassie')
 st.write("Seleccione el conjunto de imágenes a segmentar y clasificar")
 
 #browse files
-uploaded_files = st.file_uploader("Cargar Imágenes", accept_multiple_files=True, help="Solo se aceptan imágenes de formato .png y .jpg", type=(‘png’, ‘jpg’'))
+uploaded_files = st.file_uploader("Cargar Imágenes", accept_multiple_files=True, help="Solo se aceptan imágenes de formato .png y .jpg", type=[‘png’, ‘jpg’])
 #st.write(uploaded_files)
 i = 0
 for file in uploaded_files:
