ba1e680cc6f623e250acf28ca5fc82c806ce6689
SolutionsAI <86385390+SolutionsAI@users.noreply.github.com>
2021-07-28 10:35:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ if i:
           
      #st.write(os.getcwd()) #para ver el directorio
      j = detectHOS(C/100)
-     st.write("termiando con exito)
+     st.write("termiando con exito")
      #img = cv2.imread("runs/detect/exp/burro.jpg")
      #st.image(img[:,:,::-1])
 
