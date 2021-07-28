8d4b334415bd5406f745cacdb7b35541b38763e9
SolutionsAI <86385390+SolutionsAI@users.noreply.github.com>
2021-07-28 10:15:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -55,8 +55,9 @@ st.write("Seleccione el conjunto de imágenes a segmentar y clasificar")
 #browse files
 uploaded_files = st.file_uploader("Cargar Imágenes", accept_multiple_files=True, help="Solo se aceptan imágenes en formato .png y .jpg", type=["png", "jpg", "jpeg"])
 #st.write(os.listdir("/app/detecthc"))
+largo = [len(uploaded_files)]
 #st.write(len(uploaded_files))
-for i in len(uploaded_files):
+for i in largo:
      st.write(uploaded_files[i])
      
 #if uploaded_files is not None:
